b22d216b0dcc53dce0e1181f2bb84ea4cfebcc36
Muhammad Yusuf Islam <muhammadyusuf7360@gmail.com>
2025-03-23 23:38:40 +0700

    refactor: restructure project folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
 
+# firebase
+import firebase_admin
+from firebase_admin import credentials, firestore
+
 app = Flask(__name__)
 
-from app import routes
+# Load Credentials
+cred = credentials.Certificate(".\cred\cpsystem-adminsdk.json")
+app_primary = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# Import and register routes at the end to avoid circular imports
+from app.routes import routes  
+app.register_blueprint(routes)
--- app/routes.py
+++ app/routes.py
@@ -1,185 +1,23 @@
-from app import app
-from flask import request, redirect, render_template
-import os
-import uuid
-from app import db
+from flask import Blueprint, render_template
+
+routes = Blueprint("routes", __name__)
+from app import db, app
 
 # Pages
 @app.route('/')
-def home():
+@app.route('/index')
+def index():
     # Ambil Halaman HTML Home
     return render_template('index.html')
 
 
-@app.route('/LihatBarang')
+@app.route('/items')
 def lihatBarang():
-    # Akses Variabel Global
-    # global db
-
-    # # Ambil Halaman HTML LihatBarang
-    # file = open("./templates/read.html", "r")
-    # page = file.read()
-    # file.close()
-
-    # Ambil Data dari Database
     documents = db.collection("Inventori").get()
-    # data = "" # Kontainer Data ke HTML
-    # if(len(documents)>0):
-    #     # Jika Ada Data, Masukkan Data ke String HTML
-    #     for document in documents:
-    #         data += "<tr>" # Pembukaan Row HTML
-    #         data += f"<td>{document.id} - " \
-    #                 f"<a href='/UbahBarang?idbarang={document.id}'>Ubah</a> - " \
-    #                 f"<a href='/HapusBarang?idbarang={document.id}'>Hapus</a></td>"
-    #         data += f"<td>{document.to_dict()['nama']}</td>"
-    #         data += f"<td>{document.to_dict()['jumlah']}</td>"
-    #         data += f"<td>{document.to_dict()['deskripsi']}</td>"
-    #         data += "</tr>" # Penutupan Row HTML
-    # else:
-    #     # Jika Tidak Ada Data, Tampilkan Kosong
-    #     data = "<tr><td colspan='4'>Data Kosong</td></tr>"
-    # page = page.replace("<tr>Data</tr>", data)
-
-    # Konversi Data Firestore ke Format List
-    data = []
-    for document in documents:
-        item = document.to_dict()
-        item["id"] = document.id  # Simpan ID dokumen
-        data.append(item)
+    arr_data = []
+    for doc in documents:
+        data = doc.to_dict() # Convert document to dictionary
+        data["id"] = doc.id
+        arr_data.append(data)
     
-    # Render Template dengan Data
-    return render_template("read.html", data=data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.route('/TambahBarang')
-# def tambahBarang():
-#     # Akses Variabel Global
-#     global db
-#     # UUID Generator -> Kapital -> Potong 10 Karakter
-#     idBarang = str(uuid.uuid4()).replace("-","").upper()[0:10]
-#     # Ambil Halaman HTML TambahBarang
-#     file = open("./static/add.html", "r")
-#     page = file.read()
-#     file.close()
-#     # Modifikasi input untuk ID Barang di HTML
-#     page = page.replace("IDBARANG", idBarang)
-#     # Tampilkan HTML dengan Return
-#     return page
-
-# @app.route('/kirimData', methods=["POST"])
-# def kirimData():
-#     # Ambil Data dari Form
-#     idBarang = str(request.form.get('id'))
-#     namaBarang = str(request.form.get('nama'))
-#     jumlahBarang = str(request.form.get('jumlah'))
-#     deskripsiBarang = str(request.form.get('deskripsi'))
-#     # Masukkan ke Struktur Data
-#     data = {
-#         'id': idBarang,
-#         'nama': namaBarang,
-#         'jumlah': jumlahBarang,
-#         'deskripsi': deskripsiBarang
-#     }
-#     # Kueri ke Database
-#     db.collection('Inventori').document('Doc-'+idBarang).set(data)
-#     # Tampilkan Lihat Barang
-#     return redirect("/LihatBarang")
-
-
-# @app.route('/UbahBarang')
-# def ubahBarang():
-#     # Akses Variabel Global
-#     global db
-#     # Ambil ID Barang dari GET Request
-#     idBarang = request.args.get('idbarang')
-#     # Ambil Data di Database
-#     data = db.collection("Inventori").document(idBarang).get().to_dict()
-#     namaBarang = data['nama']
-#     jumlahBarang = data['jumlah']
-#     deskripsiBarang = data['deskripsi']
-#     # Ambil Halaman HTML TambahBarang
-#     file = open("static/update.html", "r")
-#     page = file.read()
-#     file.close()
-#     # Modifikasi input di HTML
-#     page = page.replace("IDBARANG", idBarang.replace("Doc-",""))
-#     page = page.replace("NAMABARANG", namaBarang)
-#     page = page.replace("JUMLAHBARANG", jumlahBarang)
-#     page = page.replace("DESKRIPSIBARANG", deskripsiBarang)
-#     # Tampilkan HTML dengan Return
-#     return page
-
-
-# @app.route('/kirimPerubahan', methods=["POST"])
-# def kirimPerubahan():
-#     # Ambil Data dari Form
-#     idBarang = str(request.form.get('id'))
-#     namaBarang = str(request.form.get('nama'))
-#     jumlahBarang = str(request.form.get('jumlah'))
-#     deskripsiBarang = str(request.form.get('deskripsi'))
-#     # Masukkan ke Struktur Data
-#     data = {
-#         'id': idBarang,
-#         'nama': namaBarang,
-#         'jumlah': jumlahBarang,
-#         'deskripsi': deskripsiBarang
-#     }
-#     # Kueri ke Database
-#     db.collection('Inventori').document('Doc-'+idBarang).set(data)
-#     # Tampilkan Lihat Barang
-#     return redirect("/LihatBarang")
-
-
-# @app.route('/HapusBarang')
-# def hapusBarang():
-#     # Akses Variabel Global
-#     global db
-#     # Ambil ID Barang dari GET Request
-#     idBarang = request.args.get('idbarang')
-#     # Ambil Data di Database
-#     data = db.collection("Inventori").document(idBarang).get().to_dict()
-#     namaBarang = data['nama']
-#     jumlahBarang = data['jumlah']
-#     deskripsiBarang = data['deskripsi']
-#     # Ambil Halaman HTML TambahBarang
-#     file = open("WebApp/html/delete.html", "r")
-#     page = file.read()
-#     file.close()
-#     # Modifikasi input di HTML
-#     page = page.replace("IDBARANG", idBarang.replace("Doc-",""))
-#     page = page.replace("NAMABARANG", namaBarang)
-#     page = page.replace("JUMLAHBARANG", jumlahBarang)
-#     page = page.replace("DESKRIPSIBARANG", deskripsiBarang)
-#     # Tampilkan HTML dengan Return
-#     return page
-
-
-# @app.route('/hapusData', methods=["POST"])
-# def hapusData():
-#     # Ambil Data dari Form
-#     idBarang = str(request.form.get('id'))
-#     # Kueri ke Database
-#     db.collection('Inventori').document('Doc-'+idBarang).delete()
-#     # Tampilkan Lihat Barang
-#     return redirect("/LihatBarang")
+    return render_template("read-item.html", arr_data=arr_data)
